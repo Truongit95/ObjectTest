04e4520ba35b594bff62cd9d5f215a6b20aec6db
Nguyen Nhut Truong <nguyennhuttruong95@gmail.com>
2017-07-20 22:43:52 +0700

    update source

--- public/js/Controller/Home/homeCtrl.js
+++ public/js/Controller/Home/homeCtrl.js
@@ -10,11 +10,82 @@
     function homeCtrl($rootScope, $scope, apiService, SweetAlert, $state, $cookies) {
         $rootScope.isLogin = true;
         //kiem tra da dang nhap hay chua
-        if ($cookies.get('isStatusLogin') == 1) {
-            //da dang nhap roi
-        } else {
-            //chua dang nhap
+        $scope.getlistALL=function(){
+            apiService.apiGet('/api/subjects',null,success,fail);
         }
+        function success(response){
+            $scope.monhoc=response.data;
+        }
+        function fail(response){
+
+        }
+        $scope.getlistALL();
+        $scope.goquiz=function(idmon,name){
+            if($cookies.get('isStatusLogin')!=1)
+            {
+                //chua dang nhap
+                 swal({
+                    title: "Thông báo",
+                    text: "Đăng nhập để thực hiện chức năng này!",
+                    type: "warning",
+                    showCancelButton: true,
+                    confirmButtonText: "Đồng ý",
+                    closeOnConfirm: false,
+                    closeOnCancel: true
+                }, function(isConfirm) {
+                    if (isConfirm) {
+                        swal.close();
+                        $rootScope.isLogin = false;
+                        $state.go('login');
+                    } else {
+                        $state.go('home');
+                    }
+                })
+            }else{
+                //dang nhap roi tạo de thi
+                $scope.idmon=idmon;
+                $scope.name=name;
+                apiService.apiGet('/api/subjects/completed/'+$cookies.getObject('datauser').Id,null,checksuccess,checkfail);
+            }
+        }
+        function checksuccess(response){
+              if(response.data!=null){
+                   toastr.warning('Bạn đã tạo đề trước đó, vui lòng hoàn thành xong nhé!', 'Thông báo!');
+                    $state.go('detailsexam');
+              }else{
+                   $scope.taode( $scope.idmon, $scope.idmon);
+              }
+        };
+        function checkfail(response){};
+        $scope.taode=function(idmon,name){
+              $.blockUI({
+                message: '<h2>Đang Tạo đề thi. Vui lòng chờ!</h2>',
+            });
+            setTimeout(function() {
+                $.unblockUI({
+                    onUnblock: function() {
+                        //goi api yeu cau tao de thi
+                        //neu thang cong thi tra ve ctdt
+                        //tao cookies luu id de thi
+                        var da={
+                            "number":'20', // số câu cần tạo cho 1 đề
+                            "subjectId":idmon,  // id môn học cần tạo đề
+                            "subjectName":name+" "+$cookies.getObject('datauser').Id+" "+20,   // tiêu đề cho đề thi
+                            "userId":$cookies.getObject('datauser').Id  // đề thi này của user nào
+                        };
+                        apiService.apiPost('/api/subjects/generatesheet',da,null,createsucs,createfail);  
+                    }
+                });
+            }, 3000);
+        };
+        function createsucs(response){
+            toastr.success(response.data.message, 'Thông báo!');
+            // tao xong thi den trang chi tiet de thi de load chi tiet
+            sessionStorage.isStatusquiz = 0;
+            $state.go('detailsexam');
+            $cookies.put('isstatus', 0);
+        };
+        function createfail(response){};
     }
 
 })();
